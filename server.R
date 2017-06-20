90e1decfbb12e06b523bb5ef7fb680fe06df38b5
rpri <reshu.priya007@gmail.com>
2017-06-20 19:30:46 +0530

    Final commit

---
+++ server.R
@@ -0,0 +1,42 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+
+# Depending on user's input for x-parameter, this app calculates mean Toothlength for that parameter and plots it for various levels
+
+shinyServer(function(input, output) {
+   
+  output$distPlot <- renderPlot({
+  
+    #display plot by dosage
+   if(input$parameter==1)
+   { # calculate aggregate toothlength  growth for every dosage and supplement type to plot
+     avg <- aggregate(len~.,data=ToothGrowth,mean)
+    g <- ggplot(aes(x=dose, y = len), data =avg) + 
+      geom_point(aes(color = supp)) 
+    g <- g + geom_line(data=ToothGrowth,aes(group=supp,colour=supp))+ggtitle("Plot showing Toothlength growth Vs Dosage")}
+    
+    #display plot by supplement type
+    if(input$parameter==2)
+      {g<-ggplot(aes(x = supp, y = len), data=ToothGrowth) + 
+      geom_boxplot(aes(fill = supp))+ggtitle("Plot showing Toothlength growth Vs supp")}
+    
+    #display plot by supplement type and dosage
+    if (input$parameter==3)
+    {g<-ggplot(aes(x=supp, y=len),data = ToothGrowth) +
+        geom_boxplot(aes(fill=supp))+facet_wrap(~ dose)+ggtitle("Plot showing Toothlength Vs supp for every dose ")
+        }
+    print(g)
+    
+  
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,28 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+#This app plots Toothlength growth by calculating mean for various x-parameters selected by user 
+# Define UI for application that draws a plot depending on user's input
+shinyUI(fluidPage(
+  
+  # Tooth length growth analysis 
+  titlePanel("Tooth length growth analysis for different parameters"),
+  
+  # Sidebar with a dropdown list input for selecting x-axis parameter to show on Toothlength growth plot
+  sidebarLayout(
+    sidebarPanel(
+       selectInput("parameter",label=h5("Select which x-parameter you wish to Plot"),
+                   ,choices=list("Dosage"=1,"Supplement"=2,"Both Supplement and dosage"=3),selected=1)),
+    # Show a plot of the Toothlength growth vs parameter selected by user
+    mainPanel(
+       plotOutput("distPlot")
+    )
+  )
+))
